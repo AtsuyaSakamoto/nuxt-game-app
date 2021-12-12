d1db6d353a94709cdc3a8becdccee3fdf8a5c660
AtsuyaSakamoto <sakamo.070501@gmail.com>
2021-12-12 19:09:40 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const text = 'Hello, World!'
+
+    return {
+      text,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+// div {
+//   background-color: blue;
+// }
+</style>
